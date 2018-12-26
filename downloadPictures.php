3d0d0d57f4e5585e2582c7f9a2ce2125fb17219a
Talip Can Korkmazer <tkorkmazer@hotmail.com>
2018-12-26 11:34:51 +0300

    Update downloadPictures.php

--- downloadPictures.php
+++ downloadPictures.php
@@ -5,7 +5,7 @@ set_time_limit(0);
 require 'vendor/autoload.php';
 
 $tokenLib = new DropboxClient\tokenLib;
-$tokenLib::init();
+$tokenLib->init();
 
 $root = dirname((__FILE__));
 
@@ -17,13 +17,13 @@ $dropbox = new DropboxClient\DropboxClient(array(
 
 $return_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] . "?auth_redirect=1";
 
-$bearer_token = $tokenLib::loadToken("bearer");
+$bearer_token = $tokenLib->loadToken("bearer");
 
 if ($bearer_token) {
     $dropbox->SetBearerToken($bearer_token);
 } elseif (!empty($_GET['auth_redirect'])) {
     $bearer_token = $dropbox->GetBearerToken(null, $return_url);
-    $tokenLib::storeToken($bearer_token, "bearer");
+    $tokenLib->storeToken($bearer_token, "bearer");
 } elseif (!$dropbox->IsAuthorized()) {
     $auth_url = $dropbox->BuildAuthorizeUrl($return_url);
     header('Location: ' . $auth_url);
